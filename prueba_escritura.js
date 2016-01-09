f7aae91c5a966ea0d8d7f7346927408299c18039
Sergio Pola <sergio.pola.contreras@gmail.com>
2016-01-09 19:56:44 -0300

    Prueba de escritura en el mongo usando mongoose

--- prueba_escritura.js
+++ prueba_escritura.js
@@ -2,23 +2,63 @@
 /*jslint unparam:true*/
 'use strict';
 var mongoose = require('mongoose'),
+    Ruta = require('./models/ruta'),
     Despacho = require('./models/despacho');
 
 
-mongoose.connect('mongodb://192.168.1.39/despacho', function (err, res) {
+mongoose.connect('mongodb://localhost/prefijo', function (err, res) {
     if (err) {
         console.log('ERROR: connecting to Database. ' + err);
     }
+
+    var ruta = new Ruta({
+        nombre: "ruta2",
+        descripcion: "descripcion de la ruta",
+        estado: 1,
+        fechas: {
+            creacion: "2015-10-04",
+            termino: ""
+        },
+        direccion: {
+            ciudad: "Valparaiso",
+            region: "Playa ancha",
+            detalle: "Av.Libertador babasdas"
+        },
+        carga: {
+            tipo: "Pescado",
+            peso: 700
+        },
+        oferta: 2000000,
+        empresa: {
+            nombre: "Don Lucho",
+            logo: "donlucho"
+        }
+    });
 
     var despacho = new Despacho({
         nombre: "Mi despacho",
         descripcion: "Un despacho amplio y lindo",
         estado: 1
     });
-
-    despacho.save(function (err, despacho) {
-        Despacho.find({}, function (err, despachos) {
-            console.info(despachos);
+    
+    var cb = function() {
+        var despachos = Despacho.find({}, function(err, data){
+            console.info(data);
         });
-    });
+        var rutas = Ruta.find({}, function(err, data){
+            console.info(data);
+        });
+    };
+    var done = function() {
+        done.executed +=1;
+        
+        if(done.executed === 2) {
+            cb();
+        }
+    };
+    done.executed = 0;
+    
+    ruta.save(done);
+
+    despacho.save(done);
 });
